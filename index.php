3cb7271e3810fbc96666db76b5e8902732126d80
Emmedson <livinusdogz@gmail.com>
2022-12-04 18:08:23 +0100

    fir project

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$x = 500;
+
+$x = array("Isreal", "Biodun", "Timi", "Emmanuel");
+
+
+// This is a carmel case
+$studentName = "Julian";
+
+$student_name = "Mike";
+
+$_411 = "update information";
+
+print_r($x);
+
+echo"<br>";
+
+echo $x[0];
+
+$x == $y
+ 
+
+// echo "<br>$x";
+
+?>
